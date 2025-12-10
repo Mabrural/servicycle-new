aea1a95a1fb7668921e918675354cd877ca9943f
mabrural <mabruralmutaqi@gmail.com>
2025-12-10 23:25:08 +0700

    perubahan ux pada create kendaraan agar kalau ada error tampil data old

--- resources/views/vehicle/create.blade.php
+++ resources/views/vehicle/create.blade.php
@@ -12,6 +12,18 @@
                     </h4>
                     <p class="text-muted mb-4">Silakan isi informasi kendaraan dengan lengkap.</p>
 
+                    {{-- ALERT ERROR --}}
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <strong>Terjadi kesalahan:</strong>
+                            <ul class="mt-2 mb-0">
+                                @foreach ($errors->all() as $err)
+                                    <li>{{ $err }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     <form action="{{ route('vehicle.store') }}" method="POST">
                         @csrf
 
@@ -22,14 +34,22 @@
                         <div class="row">
                             <div class="col-md-6 mb-3">
                                 <label class="form-label fw-semibold">Merek {{ ucfirst($type) }}</label>
-                                <input type="text" name="brand" class="form-control"
-                                    placeholder="Contoh: Toyota, Honda" required>
+                                <input type="text" name="brand"
+                                    class="form-control @error('brand') is-invalid @enderror"
+                                    placeholder="Contoh: Toyota, Honda" value="{{ old('brand') }}" required>
+                                @error('brand')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
 
                             <div class="col-md-6 mb-3">
                                 <label class="form-label fw-semibold">Model</label>
-                                <input type="text" name="model" class="form-control" placeholder="Contoh: Avanza, Beat"
-                                    required>
+                                <input type="text" name="model"
+                                    class="form-control @error('model') is-invalid @enderror"
+                                    placeholder="Contoh: Avanza, Beat" value="{{ old('model') }}" required>
+                                @error('model')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
 
@@ -37,14 +57,22 @@
                         <div class="row">
                             <div class="col-md-6 mb-3">
                                 <label class="form-label fw-semibold">Tahun Kendaraan</label>
-                                <input type="number" name="tahun" class="form-control" placeholder="Contoh: 2020"
-                                    required>
+                                <input type="number" name="tahun"
+                                    class="form-control @error('tahun') is-invalid @enderror" placeholder="Contoh: 2020"
+                                    value="{{ old('tahun') }}" required>
+                                @error('tahun')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
 
                             <div class="col-md-6 mb-3">
                                 <label class="form-label fw-semibold">Nomor Plat</label>
-                                <input type="text" name="plate_number" class="form-control"
-                                    placeholder="Contoh: BP 1234 XX" required>
+                                <input type="text" name="plate_number"
+                                    class="form-control @error('plate_number') is-invalid @enderror"
+                                    placeholder="Contoh: BP 1234 XX" value="{{ old('plate_number') }}" required>
+                                @error('plate_number')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
 
@@ -52,13 +80,22 @@
                         <div class="row">
                             <div class="col-md-6 mb-3">
                                 <label class="form-label fw-semibold">Kilometer</label>
-                                <input type="number" name="kilometer" class="form-control" placeholder="Contoh: 12000"
-                                    required>
+                                <input type="number" name="kilometer"
+                                    class="form-control @error('kilometer') is-invalid @enderror"
+                                    placeholder="Contoh: 12000" value="{{ old('kilometer') }}" required>
+                                @error('kilometer')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
 
                             <div class="col-md-6 mb-3">
                                 <label class="form-label fw-semibold">Masa Berlaku STNK</label>
-                                <input type="date" name="masa_berlaku_stnk" class="form-control" required>
+                                <input type="date" name="masa_berlaku_stnk"
+                                    class="form-control @error('masa_berlaku_stnk') is-invalid @enderror"
+                                    value="{{ old('masa_berlaku_stnk') }}" required>
+                                @error('masa_berlaku_stnk')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
 
