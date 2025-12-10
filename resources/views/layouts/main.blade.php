9408acdbeb5af1376c983093069866a6abcf2e38
mabrural <mabruralmutaqi@gmail.com>
2025-12-10 23:19:36 +0700

    penambahan sweetalert2 pada main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
 
 <body class="with-welcome-text">
     <div class="container-scroller">
-        
+
         @include('layouts.navbar')
         <!-- partial -->
         <div class="container-fluid page-body-wrapper">
@@ -18,6 +18,38 @@
     </div>
     <!-- container-scroller -->
     @include('layouts.script')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        @if (session('success'))
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil!',
+                text: "{{ session('success') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        @endif
+
+        @if (session('error'))
+            Swal.fire({
+                icon: 'error',
+                title: 'Gagal!',
+                text: "{{ session('error') }}",
+                showConfirmButton: true
+            });
+        @endif
+
+        @if ($errors->any())
+            Swal.fire({
+                icon: 'warning',
+                title: 'Validasi Gagal',
+                html: `{!! implode('<br>', $errors->all()) !!}`,
+                confirmButtonColor: '#d33',
+            });
+        @endif
+    </script>
+
+
 </body>
 
 </html>
