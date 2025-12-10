2548e20adf1b4034c44642b07419c2955201712b
mabrural <mabruralmutaqi@gmail.com>
2025-12-10 15:25:49 +0700

    login sudah berhasil masuk menggunakan template new

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,51 +2,86 @@
 
 @section('container')
 <div class="container-scroller">
-        <div class="container-fluid page-body-wrapper full-page-wrapper">
-            <div class="content-wrapper d-flex align-items-center auth px-0">
-                <div class="row w-100 mx-0">
-                    <div class="col-lg-4 mx-auto">
-                        <div class="auth-form-light text-left py-5 px-4 px-sm-5">
-                            <div class="brand-logo">
-                                <img src="../../assets/images/logo.svg" alt="logo">
-                            </div>
-                            <h4>Hello! let's get started</h4>
-                            <h6 class="fw-light">Sign in to continue.</h6>
-                            <form class="pt-3">
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1"
-                                        placeholder="Username">
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-lg"
-                                        id="exampleInputPassword1" placeholder="Password">
-                                </div>
-                                <div class="mt-3 d-grid gap-2">
-                                    <a class="btn btn-block btn-primary btn-lg fw-medium auth-form-btn"
-                                        href="../../index.html">SIGN IN</a>
-                                </div>
-                                <div class="my-2 d-flex justify-content-between align-items-center">
-                                    <div class="form-check">
-                                        <label class="form-check-label text-muted">
-                                            <input type="checkbox" class="form-check-input"> Keep me signed in </label>
-                                    </div>
-                                    <a href="#" class="auth-link text-black">Forgot password?</a>
-                                </div>
-                                <div class="mb-2 d-grid gap-2">
-                                    <button type="button" class="btn btn-block btn-facebook auth-form-btn">
-                                        <i class="ti-facebook me-2"></i>Connect using facebook </button>
-                                </div>
-                                <div class="text-center mt-4 fw-light"> Don't have an account? <a href="register.html"
-                                        class="text-primary">Create</a>
-                                </div>
-                            </form>
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth px-0">
+            <div class="row w-100 mx-0">
+                <div class="col-lg-4 mx-auto">
+                    <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                        <div class="brand-logo">
+                            <img src="../../assets/images/logo.svg" alt="logo">
                         </div>
+
+                        <h4>Selamat datang!</h4>
+                        <h6 class="fw-light">Silakan masuk untuk melanjutkan.</h6>
+
+                        <!-- FORM LOGIN -->
+                        <form class="pt-3" method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            {{-- Email / Username --}}
+                            <div class="form-group">
+                                <input type="email"
+                                    name="email"
+                                    class="form-control form-control-lg @error('email') is-invalid @enderror"
+                                    placeholder="Alamat Email"
+                                    value="{{ old('email') }}"
+                                    required autofocus>
+
+                                @error('email')
+                                    <div class="text-danger small mt-1">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            {{-- Password --}}
+                            <div class="form-group">
+                                <input type="password"
+                                    name="password"
+                                    class="form-control form-control-lg @error('password') is-invalid @enderror"
+                                    placeholder="Kata Sandi"
+                                    required>
+
+                                @error('password')
+                                    <div class="text-danger small mt-1">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            {{-- Tombol Sign In --}}
+                            <div class="mt-3 d-grid gap-2">
+                                <button type="submit" class="btn btn-primary btn-lg fw-medium auth-form-btn">
+                                    MASUK
+                                </button>
+                            </div>
+
+                            {{-- Remember Me + Lupa Password --}}
+                            <div class="my-2 d-flex justify-content-between align-items-center">
+
+                                <div class="form-check">
+                                    <label class="form-check-label text-muted">
+                                        <input type="checkbox" name="remember" class="form-check-input">
+                                        Ingat saya
+                                    </label>
+                                </div>
+
+                                @if (Route::has('password.request'))
+                                <a href="{{ route('password.request') }}" class="auth-link text-black">
+                                    Lupa kata sandi?
+                                </a>
+                                @endif
+                            </div>
+
+                            {{-- Register --}}
+                            <div class="text-center mt-4 fw-light">
+                                Belum punya akun?
+                                <a href="{{ route('register') }}" class="text-primary">Daftar</a>
+                            </div>
+
+                        </form>
+                        <!-- END FORM -->
                     </div>
                 </div>
             </div>
-            <!-- content-wrapper ends -->
         </div>
-        <!-- page-body-wrapper ends -->
+        <!-- content-wrapper ends -->
     </div>
-
+</div>
 @endsection
