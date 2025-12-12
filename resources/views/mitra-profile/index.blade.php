28be699a0a67018ddbab3230b28bb46c243def17
mabrural <mabruralmutaqi@gmail.com>
2025-12-13 00:38:22 +0700

    perubahan pada profile.mitra agar ada alert jika profil belum lengkap, namun memang belum lengkap sih

--- resources/views/mitra-profile/index.blade.php
+++ resources/views/mitra-profile/index.blade.php
@@ -15,6 +15,11 @@
                                 </li>
                             </ul>
                         </div>
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>Data Profil Belum Lengkap.</strong>
+                            Untuk dapat diproses lebih lanjut, mohon lengkapi seluruh informasi profil bengkel Anda.
+                            <a href="{{ route('profile.mitra') }}">Lengkapi profil sekarang</a>.
+                        </div>
 
                         <div class="tab-content tab-content-basic">
                             <div class="tab-pane fade show active" id="overview" role="tabpanel">
