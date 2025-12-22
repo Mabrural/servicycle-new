c2c6196ce31f83db6759d2fbaa42eacbfd50d518
mabrural <mabruralmutaqi@gmail.com>
2025-12-22 21:13:09 +0700

    perubahan pada status style agar warnanya ungu

--- resources/views/auth/layouts/head.blade.php
+++ resources/views/auth/layouts/head.blade.php
@@ -19,4 +19,34 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <!-- endinject -->
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
+
+    <style>
+        :root {
+            --sc-primary: #4f46e5;
+            --sc-primary-dark: #4338ca;
+        }
+
+        /* override bootstrap primary */
+        .bg-primary {
+            background-color: var(--sc-primary) !important;
+        }
+
+        .btn-primary {
+            background-color: var(--sc-primary);
+            border-color: var(--sc-primary);
+        }
+
+        .btn-primary:hover {
+            background-color: var(--sc-primary-dark);
+            border-color: var(--sc-primary-dark);
+        }
+
+        .text-primary {
+            color: var(--sc-primary) !important;
+        }
+
+        .nav-pills .nav-link.active {
+            background-color: var(--sc-primary);
+        }
+    </style>
 </head>
