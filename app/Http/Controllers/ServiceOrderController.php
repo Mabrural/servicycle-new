91c52f6c7d1aedfd09ae6cfd78ce04386a360720
mabrural <mabruralmutaqi@gmail.com>
2025-12-26 13:48:59 +0700

    penghapusan komentar tidak perlu di service order

--- app/Http/Controllers/ServiceOrderController.php
+++ app/Http/Controllers/ServiceOrderController.php
@@ -376,37 +376,6 @@ class ServiceOrderController extends Controller
             ->with('success', 'Customer berhasil check-in');
     }
 
-    // public function enqueue(ServiceOrder $order)
-    // {
-    //     $mitra = auth()->user()->mitra;
-
-    //     // Validasi kepemilikan
-    //     if (!$mitra || $order->mitra_id !== $mitra->id) {
-    //         abort(403);
-    //     }
-
-    //     // Validasi status
-    //     if ($order->status !== 'checked_in') {
-    //         abort(403, 'Order belum check-in');
-    //     }
-
-    //     // Tentukan nomor antrian berikutnya
-    //     $lastQueue = ServiceOrder::where('mitra_id', $mitra->id)
-    //         ->whereIn('status', ['waiting', 'in_progress'])
-    //         ->max('queue_number');
-
-    //     $nextQueueNumber = ($lastQueue ?? 0) + 1;
-
-    //     // Update order
-    //     $order->update([
-    //         'status' => 'waiting',
-    //         'queue_number' => $nextQueueNumber,
-    //     ]);
-
-    //     return redirect()
-    //         ->route('service-orders.index', ['tab' => 'queue'])
-    //         ->with('success', 'Order berhasil dimasukkan ke antrian');
-    // }
     public function enqueue(ServiceOrder $order)
     {
         $user = auth()->user();
